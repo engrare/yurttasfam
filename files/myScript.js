19858687cb9b7999f75020e20c9109acb9613e0b
engrare <147000117+engrare@users.noreply.github.com>
2025-02-01 17:30:08 +0100

    Add files via upload

--- files/myScript.js
+++ files/myScript.js
@@ -2,95 +2,31 @@
 var ismenuopen = false;
 var st;
 var window_height, window_width, old_active_index = 0;
-var is_mobile_phone = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
-
-
-var website_data_obj;
-
-fetch('https://raw.githubusercontent.com/eylulberil/engrare-data/main/data.json')
-  .then(response => response.json())
-  .then(myObj => {
-	website_data_obj = myObj;
-	var objlen = website_data_obj.website.corner.length;
-//console.log(objlen);
-	$( '#fixed_menu_but_0 p').multiline(website_data_obj.website.slide.name);
-	for(var i = 0; i < objlen; i++) {
-		if(i > 0) {
-			$( '#main_container_' + (i - 1)).clone().insertAfter( '#main_container_' + (i - 1) ).prop('id', 'main_container_' + i);
-		}
-		$( '#fixed_menu_but_' + (i)).clone().insertAfter('#fixed_menu_but_' + i ).prop('id', 'fixed_menu_but_' + (i + 1)).children("p").multiline(website_data_obj.website.corner[i].name).parent().removeClass("fixed_menu_button_selected").attr("onclick", "topMenuGo(" + (i + 1) + ")");
-		
-		$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .header_part_txt").multiline(website_data_obj.website.corner[i].header);
-		$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .content_part_txt").multiline(website_data_obj.website.corner[i].text);
-		$('#main_container_' + i + " .main_container_2_bg_photo_part .main_container_2_bg_photo").attr("id", "sliding_photo_" + i);
-		if(website_data_obj.website.corner[i].buttontext == "") {
-			
-			$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .go_furniture_detail_a").css("display", "none");
-			$('#main_container_' + i + " .main_container_2_text_part .sponsorship_form_iframe").attr("src", "");
-		}
-		else {
-			$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .go_furniture_detail_a").css("display", "block");
-			$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .go_furniture_detail_a .go_furniture_detail_txt").multiline(website_data_obj.website.corner[i].buttontext);
-			if(website_data_obj.website.corner[i].formheight != "") {
-				$('#main_container_' + i + " .main_container_2_text_part .go_furniture_detail_a").attr("onclick", "OpenCloseForm(" + i + ")");
-				$('#main_container_' + i + " .main_container_2_text_part .text_part_inner_cont .go_furniture_detail_a").attr("onclick", "OpenCloseForm(" + i + ")");
-				$('#main_container_' + i + " .main_container_2_text_part .sponsorship_form_iframe").attr("src", website_data_obj.website.corner[i].btnlink);
-				$('#main_container_' + i + " .main_container_2_text_part .sponsorship_form_iframe").css("height", website_data_obj.website.corner[i].formheight + "px");
-				
-			}
-		}
-		$('#main_container_' + i + " .main_container_2_bg_photo_part .main_container_2_bg_photo").attr("src", website_data_obj.website.corner[i].bgimglink);
-			
-		
-		$( '#fixed_menu_but_' + (objlen)).clone().insertAfter('#fixed_menu_but_' + (objlen) ).prop('id', 'fixed_menu_but_' + (objlen+1)).attr("onclick", "topMenuGo(" + (objlen + 1) + ")").children("p").text("İletişim");
-
-		
-	}
-	objlen = website_data_obj.website.slide.content.length;
-	
-	
-	for(var i = 0; i < objlen; i++) {
-			//$( '#main_container_' + (i - 1)).clone().appendTo( ".main_div" ).prop('id', 'main_container_' + i);
-		
-		//$('#main_container_' + i).children( ".search_result_div" ).children( ".user_username" ).text(myObj[i].username);
-		
-		$('.swiper-slide:eq(' + i + ') .swiper_slide_img').attr("src", website_data_obj.website.slide.content[i].bgimglink);
-		$('.swiper-slide:eq(' + i + ') .go_furniture_detail .go_furniture_detail_cont_1 .img_slogan_cont .img_slogan_txt').multiline(website_data_obj.website.slide.content[i].header);
-		$('.swiper-slide:eq(' + i + ') .go_furniture_detail .go_furniture_detail_cont_1 .go_furniture_detail_a .go_furniture_detail_cont_2 .go_furniture_detail_txt').multiline(website_data_obj.website.slide.content[i].buttontext);
-		$('.swiper-slide:eq(' + i + ') .go_furniture_detail .go_furniture_detail_cont_1 .go_furniture_detail_a').attr("onclick", "ScrollPart(" + website_data_obj.website.slide.content[i].gocorner + ")");
-		
-	}
-
-	//websiteJSON.website.slide.content.length
-	
-  })
-  .catch(error => {
-    // Handle any errors that occur during the fetch request
-    console.log('Error:', error);
-  });
-
-
-
-//for(let i = 0; i < website_data_obj.corner.length())
-
-
-function topMenuGo(num) {
-	$('html, body').stop();
-	ScrollPart(num);
-
-	if(ismenuopen)
-		openLeftMenu();
-}
-
-
-$.fn.multiline = function(text){
-    this.text(text);
-    this.html(this.html().replace(/\n/g,'<br/>'));
-    return this;
-}
 
 
 $( document ).ready(function() {
+	let imagePath = "files/photos/"; // Resimlerin olduğu klasör
+	let index = 1;
+
+	function loadImage() {
+		let img = new Image();
+		img.src = imagePath + index + ".jpg";
+
+		img.onload = function () {
+			$('.swiper-slide:eq(' + index + ') .swiper_slide_img').attr("src", img.src);
+			$('.swiper-slide:eq(' + index + ') .go_furniture_detail .go_furniture_detail_cont_1 .img_slogan_cont .img_slogan_txt').multiline(img.src);
+			$("#gallery").append(`<img src="${img.src}" alt="Resim ${index}">`);
+			index++;
+			loadImage(); // Sonraki resmi yükle
+		};
+
+		img.onerror = function () {
+			console.log("Tüm resimler yüklendi.");
+		};
+	}
+	
+	
+	
 	var mySwiper = new Swiper('.swiper-container', {
 		navigation: {
 			nextEl: '.swiper-button-next',
@@ -99,45 +35,8 @@ $( document ).ready(function() {
 		autoplay: {
 			delay: 3000, // change delay as needed
 		},
-		/*on: {
-			slideNextTransitionEnd: (swiper) => {
-				//console.log('SWIPED RIGHT');
-				if(!trans_click_pressed) {
-					if(current_active_index == mySwiper.slides.length - 1)
-						current_active_index = 0;
-					else
-						current_active_index++;
-				}
-				trans_click_pressed = false;
-				//new_active_index = current_active_index;//mySwiper.activeIndex;
-				changeTransClick(old_active_index, current_active_index);
-				old_active_index = current_active_index;
-			},
-			slidePrevTransitionEnd: (swiper) => {
-				//console.log('SWIPED LEFT');
-				if(!trans_click_pressed) {
-					if(current_active_index == 0)
-						current_active_index = mySwiper.slides.length - 1;
-					else
-						current_active_index--;
-				}
-				trans_click_pressed = false;
-				//new_active_index = current_active_index;//mySwiper.activeIndex;
-				changeTransClick(old_active_index, current_active_index);
-				old_active_index = current_active_index;
-			}
-		},*/
 		loop: true,
 	});
-	
-	/*$( ".left_right_buttons_swipper" ).hover(function() {
-		$(this).css({"-webkit-transform": "translateY(-5px)"});
-		$(this).addClass("swiper_button_hover");
-	}, function() {
-		$(this).css({"-webkit-transform": "translateY(0px)"});
-		$(this).removeClass("swiper_button_hover");
-	});
-	*/
 	$( ".fixed_menu_right_cont" ).hover(
   function() {
     $( ".settings_button_top" ).addClass( "fa-spin" );
@@ -147,20 +46,7 @@ $( document ).ready(function() {
 );
 	
 	
-
-
 	
-	/*
-	$(".fixed_menu_button").on('click', function(){
-		$('html, body').stop();
-		var button_index = $(this).attr('id').slice(15, 16);
-		ScrollPart(button_index);
-		//console.log($(".main_container_2:eq(" + (button_index) + ")").offset().top);
-		
-		if(ismenuopen)
-			openLeftMenu();
-		//console.log($(this).eq(1));
-	});*/
 	
 	$("#fixed_menu_but_0").on('click', function(){
 		mySwiper.slideToLoop(0);
@@ -190,46 +76,6 @@ $( document ).ready(function() {
 	
 	beReadyPage();
 	
-	
-	/*
-	// Define the function to go to the last slide from the first slide
-	function goToLastSlide() {
-		mySwiper.slideTo(mySwiper.slides.length - 1);
-	}
-
-	// Define the function to go to the first slide from the last slide
-	function goToFirstSlide() {
-		mySwiper.slideTo(0);
-	}
-
-// Add a click event listener to the first slide to go to the last slide
-	var firstSlide = document.querySelector('.swiper-slide:first-of-type');
-	firstSlide.addEventListener('click', function() {
-		if (mySwiper.activeIndex == 0) {
-			for(var i = 0; i < mySwiper.slides.length - 1; i++)
-				mySwiper.slideNext(i*30);
-			current_active_index = 3;
-			alert(mySwiper.activeIndex);
-		}
-	});
-
-	// Add a click event listener to the last slide to go to the first slide
-	var lastSlide = document.querySelector('.swiper-slide:last-of-type');
-	lastSlide.addEventListener('click', function() {
-		if (mySwiper.activeIndex == mySwiper.slides.length - 1) {
-			goToFirstSlide();
-			current_active_index = 0;
-		}
-	});*/
-	
-	$(window).scroll(function(event){
-		if($(this).scrollTop() > window_height) {
-			mySwiper.autoplay.stop();
-		}
-		else {
-			mySwiper.autoplay.start();
-		}
-	});
 	
 	var mySwiper = $(".swiper-container")[0].swiper;
 	//mySwiper.autoplay.stop();
